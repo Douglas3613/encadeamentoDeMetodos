5611f31885896e7c5a2c5969fec4c95ed804d12c
Douglas3613 <Douglas3613@users.noreply.github.com>
2020-07-16 00:19:01 -0300

    multabilidadeStringBuilder

---
+++ .vscode/MetodoString/src/multabilidade/multabilidade/src/App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        StringBuilder sb = new StringBuilder("Douglas Programando"); // stringBuilder e multavel
+      //  StringBuilder sb2 = sb.append(" Nessa Aula");
+
+        String p ="douglas programando";
+        String p2 = p.concat("aprendendo");
+        System.out.println("$$$$$$$$$$$$$$$$$$$$$");
+        System.out.println("String p : " + p);
+        System.out.println("String p2 : " + p2);
+        System.out.println("#########################");
+        System.out.println("StringBuilder : " + sb);
+       // System.out.println("StringBuilder sb2:" + "[" + sb2 + "]");
+    }
+}
